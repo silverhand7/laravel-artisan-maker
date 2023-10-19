3fb613ba76ae72b4c69396dc2b139141f0f66773
silverhand7 <refojunior5@gmail.com>
2023-10-19 20:33:27 +0800

    customizable directory not be tighted with namespace

--- src/Console/ServiceMakeCommand.php
+++ src/Console/ServiceMakeCommand.php
@@ -34,7 +34,6 @@ class ServiceMakeCommand extends GeneratorCommand
                 }
             }
 
-
             $stub = $this->files->get($this->getClassImplementInterfaceStub());
 
             return $this
