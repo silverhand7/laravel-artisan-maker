796d6f4801d79072d8b54f80fbb61c8cb37c504a
silverhand7 <refojunior5@gmail.com>
2023-12-30 01:45:26 +0800

    make service test;

--- tests/Unit/ServiceMakeTest.php
+++ tests/Unit/ServiceMakeTest.php
@@ -1,9 +1,35 @@
 <?php
 
-class ServiceMakeTest extends \Orchestra\Testbench\TestCase
+use Orchestra\Testbench\TestCase;
+use Silverhand7\LaravelArtisanMaker\LaravelArtisanMakerServiceProvider;
+
+class ServiceMakeTest extends TestCase
 {
-    public function test_example()
+    /**
+     * Get package providers.
+     *
+     * @param  \Illuminate\Foundation\Application  $app
+     * @return array<int, class-string<\Illuminate\Support\ServiceProvider>>
+     */
+    protected function getPackageProviders($app)
     {
+        return [
+            LaravelArtisanMakerServiceProvider::class,
+        ];
+    }
+
+    public function test_make_service_command()
+    {
+        $this->artisan('make:service HelloService');
         $this->assertTrue(true);
+        $this->assertFileExists(app_path('Services/HelloService.php'));
+    }
+
+    public function test_make_service_command_customized_directory()
+    {
+        $this->app['config']['artisan-maker.service_directory'] = 'app/CustomServiceDirectory';
+        $this->artisan('make:service HelloService');
+        $this->assertTrue(true);
+        $this->assertFileExists(app_path('CustomServiceDirectory/HelloService.php'));
     }
 }
